909bd1d6a5f6ee85c10bdd6ede0aea4ae0816abc
sgamlath <shgamlath@gmail.com>
2018-10-11 20:07:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
     res.sendFile('index.html');
 });
 
-app.listen(3000, function (err) {
+app.listen(process.env.PORT || 3000, function (err) {
     if (err) {
         console.error(err);
         return;
